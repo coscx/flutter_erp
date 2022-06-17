0ed28f58a28c85f2c962b7e491d4f8f7c124a9cc
coscx <710666260@qq.com>
2022-06-17 17:43:43 +0800

    first commit

--- lib/common/values/server.dart
+++ lib/common/values/server.dart
@@ -1,12 +1,8 @@
-// baidu yapi
-// const SERVER_API_URL = 'https://yapi.baidu.com/mock/41008';
+
 const SERVER_API_URL = 'https://yapi.ducafecat.tech/mock/11';
 
-const ERP_SERVER_API_URL = 'https://corestest.queqiaochina.com';
-
 const NEW_ERP_SERVER_API_URL = 'https://erptest.queqiaochina.com';
-// 本地搭建 yapi
-// const SERVER_API_URL = 'http://yapi.ducafecat.tech/mock/11';
+
 
 const IM_SERVER_HOST_URL = "mm.3dsqq.com";
 
